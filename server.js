369f5e7f370616dd212e81f201eeb833a969a4e1
Ismael Rodriguez <iscorodriguez99@gmail.com>
2026-04-13 20:11:53 -0600

    el servidor direcciona al index

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // 1. Importamos la librería que acabamos de instalar
 const express = require('express');
+const path= require('path');//libreria para manejar rutas de carpetas 
 
 // 2. Creamos una instancia de la aplicación
 const app = express();
@@ -7,12 +8,15 @@ const app = express();
 // 3. Definimos un puerto (el "departamento" donde vive nuestra app)
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname,'public'))); //si algun cliente pide un archivo busca en la carpeta public 
+
+
 // 4. Creamos una "Ruta" (Endpoint)
 // Cuando alguien entre a la raíz de nuestra dirección (/) ejecutamos esto:
 app.get('/', (req, res) => {
     // req = request (lo que el usuario nos manda)
     // res = response (lo que nosotros le contestamos)
-    res.send('<h1>¡Servidor funcionando!</h1><p>Hola Francisco, este es tu primer Back-end.</p>');
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
 // 5. Encendemos el servidor
